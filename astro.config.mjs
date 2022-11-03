208f3602b7539b44165b804720fb3ae9dc933d7f
Greg Gossett <greg@gossett.me>
2022-11-03 12:31:17 -0400

    Add Tailwind, image, MDX

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,20 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), image(), mdx()],
+  output: "server",
+  adapter: netlify()
+});
