af1ba841dcad207ea207e3468fef47c7d4395695
Greg Gossett <greg@gossett.me>
2022-11-13 20:34:58 -0500

    Add Reading Time plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,13 @@ import image from "@astrojs/image";
 // https://astro.build/config
 import mdx from "@astrojs/mdx";
 
+import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), image(), mdx()],
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+    extendDefaultPlugins: true,
+  },
 });
